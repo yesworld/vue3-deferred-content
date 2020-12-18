95157c0e8394374b43fa75781c1eab8aa35a6eab
yesworld <a.yesworld@gmail.com>
2020-12-18 16:58:23 +0300

    Added Example4

---
+++ example/components/Example4.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="example4">
+    <h2>Example 4</h2>
+    <p>Lazy load image and Transition</p>
+
+    <div class="example">
+      <observer-content
+        :auto-hide="true"
+      >
+        <transition name="fade" :appear="true">
+          <div>
+            <img src="https://placekitten.com/400/300" width="400" height="300" alt="kitten 4">
+          </div>
+        </transition>
+      </observer-content>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: 'Example4',
+})
+</script>
+
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.8s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
